7c783fc22483f68428cdef71a409305910ab5aea
kellerkichel <keller@ceicom.com.br>
2015-10-13 16:53:49 -0300

    removido console log dentro do loadcss

--- dev/vendor/loadcss/loadcss.js
+++ dev/vendor/loadcss/loadcss.js
@@ -11,7 +11,6 @@ Licensed MIT
         // stop placing duplacated styleSheets
         var ss_loaded = document.styleSheets;
         for (var i = ss_loaded.length - 1; i >= 0; i--) {
-            console.info(href);
             if (ss_loaded[i].href.indexOf(href) > -1) return false;
         }
 
