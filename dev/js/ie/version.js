9271fe2227eb414e13ccc669e45089ce983a3d7d
kellerkichel <keller@ceicom.com.br>
2015-10-16 15:07:31 -0300

    - Removido arquivos .ini - Adicionada validação para versões 11 e 12 (edge) do Internet Explorer

--- dev/js/ie/version.js
+++ dev/js/ie/version.js
@@ -1,13 +1,20 @@
 ﻿define(function () {
 
-    var getInternetExplorerVersion = function() {
+    var getInternetExplorerVersion = function () {
         var rv = 100; // Return value assumes failure.
         if (navigator.appName == 'Microsoft Internet Explorer') {
             var ua = navigator.userAgent;
             var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
             if (re.exec(ua) != null)
                 rv = parseFloat(RegExp.$1);
+        } else {
+            if (/Edge\/12./i.test(navigator.userAgent)) {
+                rv = 12;
+            } else if (/rv:11.0/i.test(navigator.userAgent)) {
+                rv = 11;
+            }
         }
+
         return rv;
     };
 
