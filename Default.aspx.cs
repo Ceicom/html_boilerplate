c593ee4719afc6c6fabda445d8abb3d7fe952358
Rômulo Innocêncio <romulodark@gmail.com>
2017-05-09 17:58:21 -0300

    Update Default.aspx.cs
    insert a litle example from Recaptcha

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,20 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        /*exemplo de código para o CAPTCHA
+        
+        #add a library
+        using System.Configuration;
+        
+        #create a string in public methods
+        public string sitekey { get; set; }
+        
+        #add config in void Page_Load
+        sitekey = ConfigurationManager.AppSettings["recaptcha-sitekey"].ToString();
+        
+        #inside form
+        <div class="g-recaptcha" data-sitekey="<%=sitekey%>"></div>
+        
+        */
     }
 }
