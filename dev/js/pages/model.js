77f22afc95002b28ef629b52284792e6750f88a3
kellerkichel <kellerkichel@hotmail.com>
2017-07-11 10:44:49 -0300

    add sample pages (form and codes) + updated recaptcha code

--- dev/js/pages/model.js
+++ dev/js/pages/model.js
@@ -5,14 +5,24 @@
     ],
     function () {
 
-        var $wrapper = $('#wrapper');
+        cfw.funcs.init();
 
+        var $wrapper = $('#wrapper');
         var getInfo = new getData();
             getInfo.wrapper = $wrapper;
             getInfo.type = $wrapper.attr('data-type') || undefined;
             getInfo.cat = $wrapper.attr('data-cat') || undefined;
-            getInfo.callback = function () { };
+            getInfo.callback = function () {
+
+                cfw.dotdotdot.init({ element: '.element' });
+                cfw.owlcarousel.init({ element: '.element' });
+                cfw.matchheight.init({ element: '.element' });
+
+            };
+
+        $(document).on('cfw_funcs_loaded', function () {
             getInfo.init();
+        });
 
     }
 )
