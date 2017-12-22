c50f8820564f398852e24d72222a06897b61f1e2
Keller Renato Kichel <kellerkichel@hotmail.com>
2017-12-22 17:29:16 -0200

    ie9 sem supporte :)

--- modulos/MasterPage.master.cs
+++ modulos/MasterPage.master.cs
@@ -9,10 +9,10 @@ public partial class modulos_MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        // valida ie < 8
+        // valida ie < 9
         if (Request.Browser.Type.ToUpper().Contains("IE"))
         {
-            if (Request.Browser.MajorVersion <= 8)
+            if (Request.Browser.MajorVersion <= 9)
                 Response.Redirect("https://whatbrowser.org/");
         }
     }
