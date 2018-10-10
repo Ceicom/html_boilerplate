234c75a357c4b87c4f2ae6d7f7386ce7c1eb419b
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2018-10-10 15:07:58 -0300

    Adicionada funcionalidade de float label nos forms

---
+++ dev/js/ux/floatLabel.js
@@ -0,0 +1,14 @@
+﻿define(
+    [
+        'jquery'
+    ],
+    () => {
+        $('.float-label input, .float-label textarea').blur(({ currentTarget }) => {
+            if ($(currentTarget).val()) {
+                $(currentTarget).parent().addClass('floating');
+                return;
+            }
+            $(currentTarget).parent().removeClass('floating');
+        });
+    }
+);
