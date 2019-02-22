6d3392cf014c097ca9b5832b031dbe4a447dff24
Keller Renato Kichel <kellerkichel@hotmail.com>
2019-02-22 16:09:31 -0300

    fix ConfigurationManager class

--- modulos/MasterPage.master.cs
+++ modulos/MasterPage.master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
 public partial class modulos_MasterPage : System.Web.UI.MasterPage
 {
